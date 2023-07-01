b4a2ae1a13e82a61a1273ec0c15a624359795ef6
Rajesh-163 <86925822+Rajesh-163@users.noreply.github.com>
2023-07-01 17:27:42 +0530

    done

---
+++ new-app/src/App.js
@@ -0,0 +1,362 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    <div className="group1">
+      <div className="poly1"></div>
+      <div className="triangle1"></div>
+     
+      <div className="triangle2"></div>
+    </div>
+    <div className="navbar">
+      <div className="logo1">logo</div>
+      <div className="other">
+        <div className="links">About</div>
+        <div className="links">Priving</div>
+        <div className="links">Review</div>
+       </div>
+    </div>
+    <div className="mice">
+      <div className="micPic"></div>
+    </div>
+    <div className="spic">
+    <div className="spicPic"></div>
+    </div>
+    <div className="frame3">
+      <div className="f31">
+          <div className="con"><h1 className="head">Learn how to launch a successful podcast</h1></div>
+          <div className="idea"><p className="p1">Lorem Ipsum is simply dummy text of the printing and typesetting in ustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p></div>
+          <div className="but">Sign up now</div>
+      </div>
+      <div className="f32">
+        <div className="img1"></div>
+        <div className="img2"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+        <div className="rad"></div>
+      </div>
+    </div>
+    <div className="setelyte"></div>
+    <div className="frame2">
+      <div className="f21">
+        <div className="box box1">
+          <h2>Interactive Features</h2>
+          <p className="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div className="box box2">
+          <h2>Interactive Features</h2>
+          <p className="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div className="box box3">
+          <h2>Interactive Features</h2>
+          <p className="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div className="box box4">
+          <h2>Interactive Features</h2>
+          <p className="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+      </div>
+      <div className="f22">
+        <div className="f221">
+          <h1>About the Course</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unkno</p>
+        </div>
+        <div className="f222">Explore now</div>
+      </div>
+    </div>
+    <div className="podcast"></div>
+    <div className="frame78">
+        <div className="group78">
+          <div className="f781">
+            <h3>Choose your plan</h3>
+            <div className="para">
+              <p>Lorem Ipsum is simply dummy text of the printing and</p>
+              <p> typesetting industry. Lorem Ipsum has been the industry's </p>
+            </div>
+            
+            <div className="buts">
+              <div className="buts1"><div className="buts11">monthly</div></div>
+              <div className="buts2">yearly</div>
+            </div>
+          </div>
+          <div className="f782">
+            <div className="card">
+              <div className="card1">
+                <h4>Basic Plan</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+              </div>
+              <div className="card2">
+                <div className="dol">$ 54</div>
+                <div className="per">/month</div>
+              </div>
+              <div className="card3">
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+              </div>
+              <div className="card4">
+                 <div className="trial">Start Free Trial</div>
+              </div>
+            </div>
+            <div className="card specialCard">
+              <div className="card1">
+                <h4 >Basic Plan</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+              </div>
+              <div className="card2">
+                <div className="dol">$ 54</div>
+                <div className="per">/month</div>
+              </div>
+              <div className="card3">
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+              </div>
+              <div className="card4">
+                 <div className="trial">Start Free Trial</div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card1">
+                <h4>Basic Plan</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+              </div>
+              <div className="card2">
+                <div className="dol">$ 54</div>
+                <div className="per">/month</div>
+              </div>
+              <div className="card3">
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+                <div className="list">
+                    <div className="dot"></div>
+                    <div className="listValue">Free access to video class</div>
+                </div>
+              </div>
+              <div className="card4">
+                 <div className="trial">Start Free Trial</div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div className="frame86">
+      <div className="reviewContainer">
+         <div className="revHead">
+           <h2>Review from customers</h2>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+         </div>
+         <div className="reviews">
+             <div className="sameRev mainReview">
+              <div className="sameDiv">
+                  <div className="imgCont">
+                    <div className="image"></div>
+                    <div className="rating">
+                      <div className="rat1">
+                        <div className="rat11">Lolla Smith</div>
+                        <div className="rat12">Microsoft</div>
+                      </div>
+                      <div className="rat2">
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
+              </div>
+             </div>
+             <div className="sameRev middleReview">
+             <div className="sameDiv">
+                  <div className="imgCont">
+                    <div className="image"></div>
+                    <div className="rating">
+                      <div className="rat1">
+                        <div className="rat11">Lolla Smith</div>
+                        <div className="rat12">Microsoft</div>
+                      </div>
+                      <div className="rat2">
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
+              </div>
+             </div>
+             <div className="sameRev lastReview">
+             <div className="arrow"></div>
+             <div className="sameDiv">
+                  <div className="imgCont">
+                    <div className="image"></div>
+                    <div className="rating">
+                      <div className="rat1">
+                        <div className="rat11">Lolla Smith</div>
+                        <div className="rat12">Microsoft</div>
+                      </div>
+                      <div className="rat2">
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                        <div className="star"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
+              </div>
+             </div>
+         </div>
+      </div>
+      <div className="scrollDots">
+        <div className="next next1"></div>
+        <div className="next"></div>
+        <div className="next"></div>
+        <div className="next"></div>
+        <div className="next"></div>
+      </div>
+    </div>
+    <div className="lastContainer">
+       <div className="polygon"></div>
+       <div className="footer">
+        <div className="hire">
+          <div className="hire1">
+            <h3>We have what you’re looking for</h3>
+             <div className="paras">
+             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+            <p>the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of</p>
+            <p>type a</p>
+             </div>
+          </div>
+          <div className="hire2">Get Started Now</div>
+        </div>
+        <div className="contact">
+          <div className="contact1">All Right Reserved @Copyright 2023</div>
+          <div className="contact2">
+            <div className="contact21">
+              <div className="contact211">Terms of Service</div>
+              <div className="contact211">Privacy Policy</div>
+              <div className="contact211">Product</div>
+            </div>
+            <div className="contact22">
+              <div className="contact220 contact221"></div>
+              <div className="contact220 contact222"></div>
+              <div className="contact220 contact223"></div>
+              <div className="contact220 contact224"></div>
+            </div>
+          </div>
+        </div>
+       </div>
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
